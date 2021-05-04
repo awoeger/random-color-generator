4bd5a39a90c02a99b13174115424d57ced3c45a5
Andrea Wöger <andrea.woeger@gmx.net>
2021-05-04 16:43:23 +0200

    Add functionality to index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+const randomColor = require('randomcolor');
+const createColor = require('my-colors');
+
+const getColoredOutput = (userInput) => {
+  let hue = 'random';
+  let luminosity = 'random';
+
+  if (userInput[2]) {
+    hue = userInput[2];
+  }
+
+  if (userInput[3]) {
+    luminosity = userInput[3];
+  }
+
+  let ranColor = randomColor({
+    luminosity: luminosity,
+    hue: hue,
+  });
+
+  const coloredOutput = createColor(ranColor);
+  const finishedOutput = coloredOutput(
+    `
+  ###############################
+  ###############################
+  ###############################
+  #####    ${ranColor}           #####
+  ###############################
+  ###############################
+  ###############################`,
+  );
+
+  return finishedOutput;
+};
+
+console.log(getColoredOutput(process.argv));
